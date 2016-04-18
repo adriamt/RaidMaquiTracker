cde436432f37aec850a8bbda879a747db9d12a05
adriamt <adriamt1@gmail.com>
2016-04-18 17:45:55 +0200

    Afegit Logo i Async Task classes

--- app/src/main/java/com/amt/raidmaquitracker/httpTask/AsyncHttpTask.java
+++ app/src/main/java/com/amt/raidmaquitracker/httpTask/AsyncHttpTask.java
@@ -2,28 +2,46 @@ package com.amt.raidmaquitracker.httpTask;
 
 import android.os.AsyncTask;
 
+/*
+ * Classe per a fer les conexions http asíncrones
+ * Accepta com a paràmetres diferents String i retorna un string
+ */
 
 public class AsyncHttpTask extends AsyncTask<String, Void, String>{
 
     private HttpHandler httpHandler;
 
+    /*
+     * Funció de handler per a l'Async Task
+     * @param httpHandler Handler del tipus HttpHandler
+     */
     public AsyncHttpTask(HttpHandler httpHandler){
         this.httpHandler = httpHandler;
     }
 
-    // Operation , XML
+    /*
+     * Funció que s'executa en background de l'Async Task. Es fa servir per cridar
+     * les diferents accions de connexió.
+     * @params com a paràmetres accepta tants Strings com es vulgui.
+     */
     @Override
     protected String doInBackground(String... arg0) {
         String resposta = "";
 
         switch (arg0[0]){
             case "Login":
+                // Accions a executar quan es vol fer login
+                // arg0[1] = user arg0[2] = token
                 resposta = new AsyncLogin().Login(arg0[1],arg0[2]);
                 return resposta;
             case "CreateSession":
+                // Accions a executar quan es vol crear sessió
+                // arg0[1] = user_id
                 resposta = new AsyncCreateSession().CreateSession(arg0[1]);
                 return resposta;
             case "SendLocation":
+                // Accions a executar quan es vol enviar localització
+                // arg0[1] = latitude arg0[2] = longitude arg0[3] = session_id arg0[4] = battery
                 resposta = new AsyncSendLocation().SendLocation(arg0[1],arg0[2],arg0[3],arg0[4]);
                 return resposta;
             default:
@@ -31,6 +49,10 @@ public class AsyncHttpTask extends AsyncTask<String, Void, String>{
         }
     }
 
+    /*
+     * Funció de la classe Async task que s'executa quan la tasca en background finalitza.
+     * @param result és el String que es retorna en el doInBackground
+     */
     @Override
     protected void onPostExecute(String result) {
         httpHandler.onResponse(result);
--- app/src/main/java/com/amt/raidmaquitracker/httpTask/HttpHandler.java
+++ app/src/main/java/com/amt/raidmaquitracker/httpTask/HttpHandler.java
@@ -2,26 +2,44 @@ package com.amt.raidmaquitracker.httpTask;
 
 
 /*
-    Handler class per a cridar l'Async Task de connexió HTTP
-
- */
+ * Handler class per a cridar l'Async Task de connexió HTTP
+*/
 
 public abstract class HttpHandler {
 
     /*
-     * Metode abstracte que es cridat quan l'Async Task acaba
-     * @return String - conte la resposta de l'Async Task
+     * Funció abstracte que es cridat quan l'Async Task acaba
+     * @param result String que conté la resposta de l'Async Task
      */
     public abstract void onResponse(String result);
 
+    /*
+     * Funció pública de la classe HttpHandler que passa els paràmetres
+     * necessaris per a fer login a través d'un Async Task
+     * @param user String per indicar l'usuari amb el que es vol fer login
+     * @param token String que indica el token per a poder fer login amb l'usuari indicat.
+     */
     public void login(String user, String token){
         new AsyncHttpTask(this).execute("Login",user,token);
     }
 
+    /*
+    *  Funció pública de la classe HttpHandler que passa els paràmetres
+    *  necessaris per a crear sessió a través d'un Async Task
+    *  @param user_id String per indicar l'id d'usuari amb el que es vol crear la sessió
+    */
     public void createSession(String user_id){
         new AsyncHttpTask(this).execute("CreateSession", user_id);
     }
 
+    /*
+    * Funció pública de la classe HttpHandler que passa els paràmetres
+    * necessaris per a enviar la localització a través d'un Async Task
+    * @param latitude String per indicar la latitud de la posició a enviar
+    * @param longitude String per indicar la longitud de la posició a enviar
+    * @param session_id String que indica l'id de sessió a la qual enviar la localització
+    * @param batery String per indicar la bateria del dispositiu que envia la localització
+    */
     public void sendLocation(String latitude, String longitude, String session_id, String battery){
         new AsyncHttpTask(this).execute("SendLocation", latitude, longitude, session_id, battery);
     }
